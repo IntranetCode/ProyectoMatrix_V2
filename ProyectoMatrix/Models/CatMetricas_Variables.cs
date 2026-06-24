dd2ae64f502190d86a52707fbf23fb7830e2b274
TI\Janely Salazar <sistemas@nutriservicios.com.mx>
2026-06-24 11:43:28 -0600

    Revert "Merge remote-tracking branch 'origin/Rama_Axel'"
    This reverts commit ae5f24453c29decca4418e43cd561528db7d559b, reversing
    changes made to f2fa06540736405a282e9e97e02ea8dbd11d35d8.

---
+++ ProyectoMatrix/Models/CatMetricas_Variables.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoMatrix.Models
+{
+    public class CatMetricas_Variables
+    {
+        [Key]
+        public int VariableID { get; set; }
+
+        [Required]
+        public int MetricaID { get; set; }
+
+        [Required]
+        public string NombreVariable { get; set; }
+        public bool EsLinea { get; set; } = false;
+
+        public string? TipoValor { get; set; }
+
+        public string? UnidadMedida { get; set; }
+
+        public string TipoCaptura { get; set; } = "Manual"; 
+        
+        public decimal? ValorFijo { get; set; }
+        
+        public string Formula { get; set; }
+        
+        //Nuevo campo para definir el tipo de agregación (SUM, AVG, etc.) para variables calculadas
+        public string? TipoAgregacion { get; set; }
+
+        // Navegación hacia la métrica "padre" a la que pertenece esta variable
+        [ForeignKey("MetricaID")]
+        public virtual CatMetricas Metrica { get; set; }
+    }
+}
--- ProyectoMatrix/Models/ModelUsuarios/Musuario.cs
+++ ProyectoMatrix/Models/ModelUsuarios/Musuario.cs
@@ -8,21 +8,21 @@ namespace ProyectoMatrix.Models
     {
         [Key]
         public int UsuarioID { get; set; }
-
       
         public string? Username { get; set; }
-
       
         public string? Contrasena { get; set; } 
 
-    
         public int RolID { get; set; }
 
-       
         public int PersonaID { get; set; }
 
         public bool Activo { get; set; }
         public DateTime FechaCreacion { get; set; }
+
+        public bool DebeCambiarPassword { get; set; }
+
+        public DateTime? FechaUltimoCambioPassword { get; set; }
 
         // Propiedades de navegación
         [ForeignKey("PersonaID")]
